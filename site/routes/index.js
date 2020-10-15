4ae1f5a6a0bbab42090b6223a6c1c3bef16a4e1a
Daniel Leguizamon <dany_9688@hotmail.com>
2020-10-15 12:33:10 -0300

    creacion con express generator + pase de html a ejs

---
+++ site/routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index',);
+});
+
+/* GET login. */
+router.get('/login', function(req, res, next) {
+  res.render('login',);
+});
+
+/* GET register */
+router.get('/register', function(req, res, next) {
+  res.render('register',);
+});
+
+/* GET productcart */
+router.get('/productcart', function(req, res, next) {
+  res.render('productcart',);
+});
+
+/* GET productdetail */
+router.get('/productdetail', function(req, res, next) {
+  res.render('productdetail',);
+});
+
+module.exports = router;
